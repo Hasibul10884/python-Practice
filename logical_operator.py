24fb695b04dfdc02180be2f7e381b48597b8656e
Hasibul10884 <hasibulhasan193@gmail.com>
2025-06-30 12:36:43 +0600

    logical

---
+++ logical_operator.py
@@ -0,0 +1,13 @@
+num1 = 30
+num2 = 50
+num3 = 20
+
+if num1 > num2 and num2 > num3:
+    print("num1 is the largest")
+    
+elif num2 > num1 and num2 > num3 :
+    print("num2 is the largest")
+    
+else:
+    print("num3 is the largest")
+    

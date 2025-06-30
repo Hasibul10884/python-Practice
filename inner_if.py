369e1cf8e57c7a4342a7c5bfa466753bf1227c47
Hasibul10884 <hasibulhasan193@gmail.com>
2025-06-30 12:13:26 +0600

    edit

--- inner_if.py
+++ inner_if.py
@@ -1,6 +1,6 @@
-num1 = 10
-num2 = 20
-num3 = 11
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
 
 if num1 > num2:
     if num1 > num3:

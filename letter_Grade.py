5ec83599c4de8c3d4676333349446457b5ac9766
Hasibul10884 <hasibulhasan193@gmail.com>
2025-06-30 13:22:44 +0600

    hh

---
+++ letter_Grade.py
@@ -0,0 +1,10 @@
+marks = 60
+
+if 80 <= marks <= 100:
+    print("Grade A+")   
+    
+elif 70 <= marks <= 79:
+    print("Grade A")
+    # elif 60 <= marks <= 69:
+elif 60 <= marks <= 69:
+    print("Grade A-")
---
+++ vlowel.py
@@ -0,0 +1,9 @@
+ch = "e"
+
+if ch == "a" or ch == "e" or ch== "i" or ch == "o" or ch == "u":
+    print("vowel")
+else:
+    print("consonat")
+
+    # This code checks if the character `ch` is a vowel (a, e, i, o, u).
+    

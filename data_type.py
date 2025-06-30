d4bf3596544c64ac76402cb726e8ae74e91e8158
Hasibul10884 <hasibulhasan193@gmail.com>
2025-06-30 11:54:29 +0600

    grade

--- data_type.py
+++ data_type.py
@@ -2,3 +2,10 @@ a = 1
 b = 7
 c = a + b 
 print(c)
+
+p = 10 // 4
+print(p)
+print(type(p))
+d = 10 / 4
+print(d)
+print(type(d))
---
+++ elif.py
@@ -0,0 +1,19 @@
+marks = 30
+
+if marks >= 80:
+    print("Grade A+")
+    
+elif marks >= 70:
+    print("Grade A")
+
+elif marks >= 60:
+    print("Grade A-")
+    
+elif marks >= 50:
+    print("Grade B")
+    
+elif marks >= 40:
+    print("Grade C")
+    
+else:
+    print("Grade Fail")

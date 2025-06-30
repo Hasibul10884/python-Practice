c0d85c801135d05e70b439404bd6f8beb5542ffb
Hasibul10884 <hasibulhasan193@gmail.com>
2025-06-30 12:12:14 +0600

    inner-if

---
+++ inner_if.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 20
+num3 = 11
+
+if num1 > num2:
+    if num1 > num3:
+        print("num1 is the largest")
+    else:
+        print("num3 is the largest")
+if num2 > num1:
+    if num2 > num3:
+        print("num2 is the largest")
+    else:
+        print("num3 is the largest")

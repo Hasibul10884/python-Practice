80990dd0e6b15f5d0381e5981f70cac87d523d91
Hasibul10884 <hasibulhasan193@gmail.com>
2025-06-30 12:21:00 +0600

    turnery

---
+++ turnery_operator.py
@@ -0,0 +1,7 @@
+num1 = 300
+num2 = 50
+
+print(num1 if num1 > num2 else num2)
+# This code uses a ternary operator to determine the larger of two numbers.
+# It prints num1 if num1 is greater than num2, otherwise it prints num2.
+# The ternary operator is a concise way to write an if-else statement in a single

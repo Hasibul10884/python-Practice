dc08903d84651ca0e835e391e976d5d89579f49e
Hasibul10884 <hasibulhasan193@gmail.com>
2025-07-12 02:55:33 +0600

    h

---
+++ math.py
@@ -0,0 +1,7 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+print("Sum:", int(num1) + int(num2))
+print("Difference:", int(num1) - int(num2))
+print("Product:", int(num1) * int(num2))
+print("Quotient:", int(num1) / int(num2))4

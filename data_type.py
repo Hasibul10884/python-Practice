5257efdea3bd7ad6a772f6f153db1057b31a1e0a
Hasibul10884 <hasibulhasan193@gmail.com>
2025-07-15 00:44:13 +0600

    data type

--- data_type.py
+++ data_type.py
@@ -1,12 +1,5 @@
-a = 1 
-b = 7
-c = a + b 
-print(c)
+x = 1j
+print(type(x))
 
-p = 10 // 4
-print(p)
-print(type(p))
-d = 10 / 4
-print(d)
-print(type(d))
-
+y = ["apple", "banana", "cherry"]
+print(type(y))
